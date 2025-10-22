e23f34369b30078229a9e696bdd47324cab859bc
nickmly <nickmly@gmail.com>
2025-10-22 08:14:32 -0400

    initial commit

---
+++ go.mod
@@ -0,0 +1,34 @@
+module gomp3
+
+go 1.25.3
+
+require (
+	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
+	github.com/charmbracelet/colorprofile v0.2.3-0.20250311203215-f60798e515dc // indirect
+	github.com/charmbracelet/lipgloss v1.1.0 // indirect
+	github.com/charmbracelet/x/ansi v0.10.1 // indirect
+	github.com/charmbracelet/x/cellbuf v0.0.13-0.20250311204145-2c3ea96c31dd // indirect
+	github.com/charmbracelet/x/term v0.2.1 // indirect
+	github.com/ebitengine/oto/v3 v3.1.0 // indirect
+	github.com/ebitengine/purego v0.7.1 // indirect
+	github.com/erikgeiser/coninput v0.0.0-20211004153227-1c3628e74d0f // indirect
+	github.com/gdamore/encoding v1.0.1 // indirect
+	github.com/gdamore/tcell/v2 v2.8.1 // indirect
+	github.com/gopxl/beep v1.4.1 // indirect
+	github.com/jfreymuth/oggvorbis v1.0.5 // indirect
+	github.com/jfreymuth/vorbis v1.0.2 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-localereader v0.0.1 // indirect
+	github.com/mattn/go-runewidth v0.0.16 // indirect
+	github.com/muesli/ansi v0.0.0-20230316100256-276c6243b2f6 // indirect
+	github.com/muesli/cancelreader v0.2.2 // indirect
+	github.com/muesli/termenv v0.16.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/rivo/tview v0.42.1-0.20250929082832-e113793670e2 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
+	golang.org/x/sys v0.36.0 // indirect
+	golang.org/x/term v0.28.0 // indirect
+	golang.org/x/text v0.21.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,182 @@
+package main
+
+import (
+	"log"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/gdamore/tcell/v2"
+	"github.com/gopxl/beep"
+	"github.com/gopxl/beep/speaker"
+	"github.com/gopxl/beep/vorbis"
+	"github.com/rivo/tview"
+)
+
+const (
+	PlayIcon  string = "⏵"
+	PauseIcon string = "⏸"
+	StopIcon  string = "⏹"
+	NextIcon  string = "⏭"
+	PrevIcon  string = "⏮"
+)
+
+type PlayerControls struct {
+	cursor  int
+	buttons []tview.Primitive
+}
+
+type PlayerState struct {
+	playing          bool
+	currentSongIndex int
+	controls         PlayerControls
+	OnInput          func(event *tcell.EventKey) *tcell.EventKey
+}
+
+func readSongListFromDir(dirPath string) ([]string, error) {
+	dir, err := os.ReadDir(dirPath)
+	if err != nil {
+		return []string{}, err
+	}
+	songList := []string{}
+	for _, entry := range dir {
+		if entry.IsDir() {
+			continue
+		}
+		fileName := entry.Name()
+		fileExtension := filepath.Ext(fileName)
+		// TODO: support .wav and .mp3
+		if fileExtension != ".ogg" {
+			continue
+		}
+		songList = append(songList, filepath.Join(dirPath, fileName))
+	}
+	return songList, nil
+}
+
+func createStreamerFromFile(filePath string, playerState PlayerState) (beep.StreamSeekCloser, *beep.Ctrl, error) {
+	f, err := os.Open(filePath)
+	if err != nil {
+		return nil, nil, err
+	}
+	streamer, format, err := vorbis.Decode(f)
+	if err != nil {
+		log.Fatal(err)
+	}
+	speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
+	ctrl := &beep.Ctrl{Streamer: beep.Loop(1, streamer), Paused: !playerState.playing}
+	speaker.Play(ctrl)
+	return streamer, ctrl, nil
+}
+
+func main() {
+	playerState := PlayerState{
+		playing:          false,
+		currentSongIndex: 0,
+	}
+	songList, err := readSongListFromDir("songs")
+	if err != nil {
+		log.Fatal(err)
+	}
+	streamer, ctrl, err := createStreamerFromFile(songList[0], playerState)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer streamer.Close()
+	newButton := func(label string) *tview.Button {
+		button := tview.NewButton(label)
+		button.SetBackgroundColor(tcell.ColorBlue)
+		button.SetBackgroundColorActivated(tcell.ColorDarkBlue)
+		button.SetLabelColorActivated(tcell.ColorWhite)
+		return button
+	}
+	app := tview.NewApplication()
+	songTextView := tview.NewTextView()
+	songTextView.SetScrollable(true)
+	songTextView.SetDynamicColors(true)
+	songTextView.SetChangedFunc(func() {
+		app.Draw()
+	})
+	songListText := ""
+	updateSongList := func(playingIndex int) {
+		songListText = ""
+		for i, songPath := range songList {
+			color := "[white]"
+			if i == playingIndex {
+				color = "[yellow]"
+			}
+			songListText += color + filepath.Base(songPath) + "\n"
+		}
+		songTextView.SetText(songListText)
+		songTextView.ScrollToHighlight()
+	}
+	updateSongList(0)
+	baseFlex := tview.NewFlex().
+		SetDirection(tview.FlexColumnCSS).
+		AddItem(songTextView, 0, 1, false)
+	baseFlex.SetBorder(true).SetTitle("Music Player")
+	playButton := newButton(PlayIcon)
+	playButton.SetSelectedFunc(func() {
+		speaker.Lock()
+		ctrl.Paused = !ctrl.Paused
+		speaker.Unlock()
+		if playerState.playing {
+			playerState.playing = false
+			playButton.SetLabel(PlayIcon)
+			return
+		} else {
+			playerState.playing = true
+			playButton.SetLabel(PauseIcon)
+			return
+		}
+	})
+	nextButton := newButton(NextIcon)
+	nextButton.SetSelectedFunc(func() {
+		playerState.currentSongIndex = (playerState.currentSongIndex + 1) % len(songList)
+		updateSongList(playerState.currentSongIndex)
+		streamer.Close()
+		streamer, ctrl, err = createStreamerFromFile(songList[playerState.currentSongIndex], playerState)
+		if err != nil {
+			log.Fatal(err)
+		}
+	})
+	prevButton := newButton(PrevIcon)
+	prevButton.SetSelectedFunc(func() {
+		playerState.currentSongIndex = (playerState.currentSongIndex - 1 + len(songList)) % len(songList)
+		updateSongList(playerState.currentSongIndex)
+		streamer.Close()
+		streamer, ctrl, err = createStreamerFromFile(songList[playerState.currentSongIndex], playerState)
+		if err != nil {
+			log.Fatal(err)
+		}
+	})
+	controls := PlayerControls{
+		cursor: 1,
+		buttons: []tview.Primitive{
+			prevButton,
+			playButton,
+			nextButton,
+		},
+	}
+	playerState.controls = controls
+	buttonsFlex := tview.NewFlex()
+	buttonsFlex.SetBorder(true)
+	for i, btn := range controls.buttons {
+		buttonsFlex.AddItem(btn, 0, 1, i == controls.cursor)
+	}
+	playerState.OnInput = func(event *tcell.EventKey) *tcell.EventKey {
+		if event.Key() == tcell.KeyRight {
+			controls.cursor = (controls.cursor + 1) % len(controls.buttons)
+			app.SetFocus(buttonsFlex.GetItem(controls.cursor))
+			return nil
+		} else if event.Key() == tcell.KeyLeft {
+			controls.cursor = (controls.cursor - 1 + len(controls.buttons)) % len(controls.buttons)
+			app.SetFocus(buttonsFlex.GetItem(controls.cursor))
+			return nil
+		}
+		return event
+	}
+	buttonsFlex.SetInputCapture(playerState.OnInput)
+	baseFlex.AddItem(buttonsFlex, 0, 1, true)
+	app.SetRoot(baseFlex, true).Run()
+}
